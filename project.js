d38a4e30cb28486b22ce2c387251ad9a2ab6b668
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-12-20 11:27:48 +0000

    Collect a bet amount and create spin function and random reel selection.

--- project.js
+++ project.js
@@ -2,6 +2,26 @@
 
 
 const prompt= require("prompt-sync") ();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
 const deposit = () => {
     while(true){
         const depositAmount = prompt("Enter a deposit amount: ");
@@ -14,13 +34,71 @@ const deposit = () => {
         }
     }
 }; 
-            const depositAmount number
-const depositAmount = deposit();
-console.log(depositAmount);
     
-    // 2. Determine number of lines to bet on
+// 2. Determine number of lines to bet on
+
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again!");
+        } else {
+            return numberOfLines;
+        }
+    }
+}; 
+
+
 // 3. Collect a bet amount
+const getBet = (balance, lines) => {
+    while(true){
+        const bet = prompt("Enter the total bet per line : ");
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again!");
+        } else {
+            return numberBet;
+        }
+    }
+}; 
+
+
 // 4. Spin the slot machine
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for (let i = 0; i < count; i++){
+            symbols.push(symbol);
+        }
+    }
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+            for (let j = 0; j < ROWS; j++){
+                 const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+                const selectedSymbol = reelSymbols[randomIndex];
+                reels[i].push(selectedSymbol);
+                reelSymbols.splice(randomIndex, 1);
+            }
+    }
+                return reels;
+};
+spin();
+
+
 // 5. Check if user won
+
+const reels = spin();
+console.log(reels);
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
+
+
 // 6. give the user their wininngs
 // 7. Play again
